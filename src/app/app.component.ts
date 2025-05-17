59e35b05561b21becdca2a479f46ed5019029b0c
ClementJosse <clement.b.josse@gmail.com>
2025-05-17 19:56:47 +0200

    base du composant home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { PlayersComponent } from './components/players/players.component';
 import { HomeComponent } from './components/home/home.component';
 import { GameComponent } from './components/game/game.component';
 import { FooterComponent } from './components/footer/footer.component';
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { PlayersComponent } from '../players/players.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent,PlayersComponent],
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
