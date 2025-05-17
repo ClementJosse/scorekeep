279f2573aa4a07504499736dbba0518cd8d1500b
ClementJosse <clement.b.josse@gmail.com>
2025-05-18 01:27:56 +0200

    base du composant d'ajout de joueurs et de réorganisation de la liste

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  standalone: true,
+  imports: [CommonModule, FormsModule, DragDropModule, NavbarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  joueurs: { nom: string }[] = [];
 
+  ajouterJoueur() {
+    this.joueurs.push({ nom: '' });
+  }
+
+
+  supprimerJoueur(index: number) {
+    this.joueurs.splice(index, 1);
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.joueurs, event.previousIndex, event.currentIndex);
+  }
 }
